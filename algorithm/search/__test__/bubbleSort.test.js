6f78e439d0fd8561ad11990762b4b08578db872f
llgzone <985395976@qq.com>
2018-03-24 20:33:23 +0800

    add bubblesort

---
+++ algorithm/search/__test__/bubbleSort.test.js
@@ -0,0 +1,11 @@
+import bubbleSort from '../bubbleSort'
+
+test("sort from small to big", () => {
+  let testArr = [1, 3, 5, 2, 9, 7]
+
+  expect(bubbleSort(testArr)).toEqual([1, 2, 3, 5, 7, 9])
+
+  testArr = [2, 4, 3, 6, 3, 9]
+
+  expect(bubbleSort(testArr)).toEqual([2, 3, 3, 4, 6, 9])
+})
